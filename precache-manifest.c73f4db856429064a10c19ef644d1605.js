1329d845d1943ba3fcb86ac5fb79150ed5b17965
gtarragona <gtarragona@switchfly.com>
2019-10-22 11:54:29 -0700

    Updates

---
+++ precache-manifest.c73f4db856429064a10c19ef644d1605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c990fb844ed7f48f87ceb0f79b4e1a6c",
+    "url": "/forecastapp/index.html"
+  },
+  {
+    "revision": "f405fb9a8902cc09cf82",
+    "url": "/forecastapp/static/css/main.0a72f240.chunk.css"
+  },
+  {
+    "revision": "b6a9a7e3950c6ed1248d",
+    "url": "/forecastapp/static/js/2.ec121b2a.chunk.js"
+  },
+  {
+    "revision": "f405fb9a8902cc09cf82",
+    "url": "/forecastapp/static/js/main.b983b7b6.chunk.js"
+  },
+  {
+    "revision": "d9b3cfd75751b7d9159c",
+    "url": "/forecastapp/static/js/runtime~main.b0ede86d.js"
+  }
+]);
